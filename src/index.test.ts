f6e14c54f4f412b7ccc6e50ca70d54a31d8784f2
Mike Stop Continues <mikestopcontinues@gmail.com>
2021-03-18 11:14:51 +0000

    Convert to typescript

---
+++ src/index.test.ts
@@ -0,0 +1,84 @@
+// import
+
+import {dateMock} from '@sitearcade/jest-preset/tools';
+
+import {
+  hash,
+  createGenerator,
+  getNanoId,
+  getDateId,
+  isId,
+  getHashId,
+} from './index';
+
+// test
+
+describe('hash(any)', () => {
+  it('resolves any object to stable hashed integer', () => {
+    expect(hash(12345))
+      .toMatchInlineSnapshot('109950804');
+    expect(hash('qwerty'))
+      .toMatchInlineSnapshot('1505860249');
+    expect(hash(true))
+      .toMatchInlineSnapshot('2087430515');
+    expect(hash({key: 'val'}))
+      .toMatchInlineSnapshot('991022805');
+    expect(hash([true, 123, 'asdfg', {key: 'val'}]))
+      .toMatchInlineSnapshot('821928462');
+  });
+});
+
+describe('getHashId(any)', () => {
+  expect(getHashId(12345))
+    .toMatchInlineSnapshot('"00000007rle4"');
+  expect(getHashId('qwerty'))
+    .toMatchInlineSnapshot('"0000001DUr2x"');
+  expect(getHashId(true))
+    .toMatchInlineSnapshot('"0000002hgE27"');
+  expect(getHashId({key: 'val'}))
+    .toMatchInlineSnapshot('"000000154eiN"');
+  expect(getHashId([true, 123, 'asdfg', {key: 'val'}]))
+    .toMatchInlineSnapshot('"0000000TCJ8G"');
+});
+
+describe('getDateId(any)', () => {
+  dateMock.advanceTo(new Date('2020-01-01'));
+
+  expect(getDateId().slice(0, 7))
+    .toStrictEqual('rMhkpLa');
+  expect(getDateId(Date.now()).slice(0, 7))
+    .toStrictEqual('rMhkpLa');
+  expect(getDateId(12345).slice(0, 7))
+    .toStrictEqual('00003d7');
+  expect(getDateId('2020-01-01').slice(0, 7))
+    .toStrictEqual('rMhkpLa');
+  expect(getDateId(NaN).slice(0, 7))
+    .toStrictEqual('rMhkpLa');
+});
+
+describe('isId(id)', () => {
+  it('knows what a valid id looks like', () => {
+    expect(isId(getNanoId())).toStrictEqual(true);
+    expect(isId(getDateId())).toStrictEqual(true);
+    expect(isId(getDateId(new Date()))).toStrictEqual(true);
+    expect(isId(getHashId(123))).toStrictEqual(true);
+    expect(isId('abc')).toStrictEqual(false);
+  });
+});
+
+describe('createGenerator(len)', () => {
+  it('instantiates generator with ids of `len` length', () => {
+    const gen = createGenerator(15);
+
+    expect(gen.idLength).toStrictEqual(15);
+
+    ['isId', 'getNanoId', 'getDateId', 'getHashId'].forEach((k) =>
+      expect(gen[k]).toBeInstanceOf(Function));
+
+    expect(gen.isId(gen.getNanoId())).toStrictEqual(true);
+    expect(gen.isId(gen.getDateId())).toStrictEqual(true);
+    expect(gen.isId(gen.getDateId(new Date()))).toStrictEqual(true);
+    expect(gen.isId(gen.getHashId(123))).toStrictEqual(true);
+    expect(gen.isId('abc')).toStrictEqual(false);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+// import
+
+import stringify from 'fast-json-stable-stringify';
+import {customAlphabet} from 'nanoid';
+import stringHash from 'string-hash';
+
+// vars
+
+const chars = '0123456789' +
+  'abcdefghijklmnopqrstuvwxyz' +
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const charLen = chars.length;
+
+const validRx = /^[\dA-Za-z]+$/;
+const dateLen = 7;
+
+// fns
+
+const hash = (v: any) => stringHash(stringify(v));
+
+const encodeAsChars = (num: number) => {
+  let res = '';
+
+  // eslint-disable-next-line fp/no-loops
+  while (num > 0) {
+    res = chars[num % charLen] + res;
+    num = Math.floor(num / charLen);
+  }
+
+  return res;
+};
+
+// class
+
+class Id {
+  idLength: number = 12;
+
+  constructor(idLength?: number) {
+    this.idLength = idLength ?? this.idLength;
+  }
+
+  isId(v: any) {
+    return typeof v === 'string' && 
+      v.length === this.idLength && 
+      validRx.test(v);
+  }
+
+  getNanoId() {
+    return customAlphabet(chars, this.idLength)();
+  }
+
+  getDateId(dt?: string | number | Date) {
+    const enc = encodeAsChars(dt ? new Date(dt).getTime() : Date.now());
+
+    return encodeAsChars(dt ? new Date(dt).getTime() : Date.now())
+      .padStart(dateLen, '0')
+      .slice(0, this.idLength)
+      .concat(customAlphabet(chars, this.idLength - dateLen)());
+  }
+
+  getHashId(v: any) {
+    return encodeAsChars(hash(v))
+      .slice(0, this.idLength)
+      .padStart(this.idLength, '0');
+  }
+}
+
+const createGenerator = (len?: number) => new Id(len);
+
+// export
+
+const id = createGenerator();
+
+export {hash, createGenerator};
+export const isId: InstanceType<typeof Id>['isId'] = id.isId.bind(id);
+export const getNanoId: InstanceType<typeof Id>['getNanoId'] = id.getNanoId.bind(id);
+export const getDateId: InstanceType<typeof Id>['getDateId'] = id.getDateId.bind(id);
+export const getHashId: InstanceType<typeof Id>['getHashId'] = id.getHashId.bind(id);
